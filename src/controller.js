657b75061ba4b6789d9da42b02fca8eccfc2758f
NicolasPucheta <nicopuche112005@gmail.com>
2025-07-09 00:08:40 -0300

    configuracion de API

---
+++ src/controller.js
@@ -0,0 +1,36 @@
+import {pool} from "./database.js";
+
+class LibroController{
+
+    /*funcion para traer todos las personas*/
+    async getAll(req, res){
+        const [result]= await pool.query('SELECT * FROM Libros');
+        res.json(result);
+    }
+    
+    /*funcion para agregar persona*/
+    async add(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros(nombre, apellido, dni) VALUES (?, ?, ?)`, 
+            [libro.nombre, libro.apellido, libro.dni]); 
+            res.json({"Id insertado": result.insertId});
+    }
+
+    /*funcion para eliminar persona*/
+    async delete(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`, [libro.id]);
+        res.json({"Registros eliminados": result.affectedRows});
+    }
+
+    /*funcion para actualizar datos*/
+    async update(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), apellido=(?),
+            dni=(?) WHERE id=(?)`, [libro.nombre, libro.apellido, libro.dni , libro.id]); 
+        res.json({"Registros Actualizados": result.changedRows});
+    
+    }
+}
+
+export const libro = new LibroController();
